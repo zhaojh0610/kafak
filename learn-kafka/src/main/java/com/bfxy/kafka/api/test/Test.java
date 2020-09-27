8e1d036904f4acfee3ef4724c387cfaa565dc8d6
zhaojh0610 <zhaojh0610@163.com>
2020-09-27 15:12:24 +0800

    zjh-9 测试类

---
+++ learn-kafka/src/main/java/com/bfxy/kafka/api/test/Test.java
@@ -0,0 +1,12 @@
+package com.bfxy.kafka.api.test;
+
+/**
+ * @author zhaojh
+ * @date 2020/9/24 15:15
+ */
+public class Test {
+    public static void main(String[] args) {
+        long a = 3600 * 6L;
+        System.out.println(a);
+    }
+}
